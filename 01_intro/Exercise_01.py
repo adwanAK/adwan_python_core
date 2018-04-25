62a4eb46961862bdcac4d45387010b07539a6b16
Caden MacKenzie <cadenmackenzie@gmail.com>
2018-04-25 19:50:48 +0800

    intro assignments

---
+++ 01_intro/Exercise_01.py
@@ -0,0 +1,11 @@
+'''
+1 - Write and execute a script that prints "hello world" to the console.
+
+2 - Using the interpreter, print "hello world!" to the console.
+
+3 - Explore the interpretor. 
+	- Execute lines with syntax error and see what the response is. What happens if you leave out a quotation or parentheses? How helpful are the error messages?
+	- Use the help() function to explore what you can do with the interpreter. For example execute help('print'). press q to exit.
+	- Use the interpretor to perform simple math. 
+	- Calculate how many seconds are in a year. 
+'''
