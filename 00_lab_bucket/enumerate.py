2b60bc74ea5d09763b4ddf1ea33c787c2706412c
martin-martin <breuss.martin@gmail.com>
2018-04-27 11:59:23 +0800

    add basic enumerate() example

--- 00_lab_bucket/enumerate.py
+++ 00_lab_bucket/enumerate.py
@@ -1,26 +1,11 @@
-# ----- reproduce the functionality of python's enumerate() ----- #
+# ----- python's in-built .enumerate() ----- #
 
-# define a function my_enumerate() that takes an iterable as input
-# and yields the element and its index
+classes = ['Intro', 'Intermediate', 'Advanced', 'Epic Hero']
 
-def my_enumerate(iterable, start=0):
-    count = start
-    for element in iterable:
-        yield count, element
-        count += 1
+for i, c in enumerate(classes):
+    print(f"{i}: {c} python")
 
-twelve_monkeys = ['James', 'Jeffrey', 'Jose', 'Scarface', 'Tiny',
-					'Kathryn', 'L.J.', 'Marilou', 'Louie', 'Teddy',
-					'Fale', 'Wayne']
 
-# compare output from custom function to built-in function
-print([x for x in my_enumerate(twelve_monkeys)])
-print([x for x in enumerate(twelve_monkeys)])
-
-## start at defined count
-# print([x for x in my_enumerate(twelve_monkeys, 1)])
-# print([x for x in enumerate(twelve_monkeys, 1)])
-
-## investigate the generator objects produced
-# print(my_enumerate(twelve_monkeys))
-# print(enumerate(twelve_monkeys))
+## less readable, but common structure in other languages:
+# for i in range(len(courses)):
+#     print(f"{i}: {courses[i]} python")
