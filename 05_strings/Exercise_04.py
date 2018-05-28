245075f04c2cfba2849513edddd16647f339c74a
Caden MacKenzie <cadenmackenzie@gmail.com>
2018-05-28 15:47:08 +0700

    strings complete

---
+++ 05_strings/Exercise_04.py
@@ -0,0 +1,5 @@
+'''
+Write a script that finds the first vowel in a a user inputted string.
+
+
+'''
---
+++ 05_strings/Exercise_06.py
@@ -0,0 +1,8 @@
+'''
+Complete exercises
+ 8.5 - Encapsulate this code in a function named count, and generalize it so that it accepts the string and the letter as arguments.
+
+ 8.6 - Rewrite this function so that instead of traversing the string, it uses the three- parameter version of find from the previous section.
+
+
+'''
