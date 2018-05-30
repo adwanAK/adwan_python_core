5215d6427858f6c4a76aa3e619c29e70ddbee359
Caden MacKenzie <cadenmackenzie@gmail.com>
2018-05-30 15:09:52 +0700

    inheritance exercise

---
+++ 12_inheritance/Exercise_01.py
@@ -0,0 +1,17 @@
+'''
+Build on Exercise 02 from objects_classes_methods. Create subclasses of
+two of the existing classes. Create a subclass of on of those so that the
+hierarchy is at least three levels.
+
+Build these classes out like we did in Exercise 02.
+
+If you cannot think of way to build on your Exercise 02 classes you can
+start from scratch here.
+
+We encourage you to be creative and try to think of an example of your own for this
+exercise but if you are stuck, some ideas include:
+
+    - A Vehicle superclass, with Truck and Motorcycle subclasses.
+    - A Restaurant superclass, with Gourmet and FastFood subclasses.
+
+'''
