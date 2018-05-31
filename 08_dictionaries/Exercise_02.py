ad86bbe3f56ed5001e4d0b80ef767071b36c957c
Caden MacKenzie <cadenmackenzie@gmail.com>
2018-05-31 16:55:36 +0700

    dict exercises

---
+++ 08_dictionaries/Exercise_02.py
@@ -0,0 +1,10 @@
+'''
+Create a new dictionary using the three dictionaries below. Then print out each key-value pair.
+
+'''
+
+
+dict_1 = {1: 1, 2: 4}
+dict_2 = {3: 9, 4: 16}
+dict_3 = {5: 25, 6: 36, 7: 49}
+
