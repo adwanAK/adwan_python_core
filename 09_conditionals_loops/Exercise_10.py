81c4e629544890daf4f3ece3310e2b74d7c96418
Caden MacKenzie <cadenmackenzie@gmail.com>
2018-05-31 17:05:42 +0700

    conditionals

---
+++ 09_conditionals_loops/Exercise_10.py
@@ -0,0 +1,6 @@
+'''
+Write a script that prints out all the squares of numbers from a user inputed lower to a user inputed upper bound.
+Use a for loop that demonstrates the use of the range function.
+
+
+'''
