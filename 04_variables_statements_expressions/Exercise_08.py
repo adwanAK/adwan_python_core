0f5fc9036c22b97ade558825bde5dd7ee78cf01b
martin-martin <breuss.martin@gmail.com>
2018-06-01 16:23:36 +0200

    improve instructions

--- 04_variables_statements_expressions/Exercise_08.py
+++ 04_variables_statements_expressions/Exercise_08.py
@@ -1,11 +1,13 @@
 '''
 Celsius to Fahrenheit:
 
-Write the necessary code to read a degree in Celsius (as a double) from the console
+Write the necessary code to read a degree in Celsius from the console
 then convert it to fahrenheit and print it to the console.
 
-	F = 9 * (C / 5) + 32;
+    F = C * 1.8 + 32
 
 Output should read like - "27.4 degrees celsius = 81.32 degrees fahrenheit"
+
+NOTE: if you get an error, look up what input() returns!
 
 '''
