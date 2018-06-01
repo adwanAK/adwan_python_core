2ec386c058552e99c1a96233aca2439a14a23fb8
martin-martin <breuss.martin@gmail.com>
2018-06-01 12:47:49 +0200

    fix typo and formatting

--- 01_intro/Exercise_01.py
+++ 01_intro/Exercise_01.py
@@ -3,9 +3,13 @@
 
 2 - Using the interpreter, print "hello world!" to the console.
 
-3 - Explore the interpretor. 
-	- Execute lines with syntax error and see what the response is. What happens if you leave out a quotation or parentheses? How helpful are the error messages?
-	- Use the help() function to explore what you can do with the interpreter. For example execute help('print'). press q to exit.
-	- Use the interpretor to perform simple math. 
-	- Calculate how many seconds are in a year. 
+3 - Explore the interpreter.
+	- Execute lines with syntax error and see what the response is.
+        * What happens if you leave out a quotation or parentheses?
+        * How helpful are the error messages?
+	- Use the help() function to explore what you can do with the interpreter.
+        For example execute help('print').
+        press q to exit.
+	- Use the interpretor to perform simple math.
+	- Calculate how many seconds are in a year.
 '''
