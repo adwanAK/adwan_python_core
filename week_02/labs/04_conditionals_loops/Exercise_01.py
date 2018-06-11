7f85c9dcaca7b77c0658842e5bb4513ac0b6164e
Martin Breuss <martin-martin@users.noreply.github.com>
2018-06-11 18:39:19 +0200

    remove solution

--- week_02/labs/04_conditionals_loops/Exercise_01.py
+++ week_02/labs/04_conditionals_loops/Exercise_01.py
@@ -6,12 +6,3 @@ Print the result.
 NOTE: We will be using the input() function. This is demonstrated below.
 
 '''
-
-#collect input from the user
-user_input = int(input("Enter a number between 1 and 1,000,000,000 \
-    to see whether is it odd or even: "))
-
-#print the number the user inputed to the console
-print(f"The user input is {user_input}")
-
-#complete the rest of the code to determine whether it is odd or even
