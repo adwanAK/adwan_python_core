0accecdb50e4310746bd3d948fc531b961fb2a8f
Martin Breuss <martin-martin@users.noreply.github.com>
2018-06-11 18:55:30 +0200

    correct exercise numbers

--- week_03/labs/06_strings/Exercise_06.py
+++ week_03/labs/06_strings/Exercise_06.py
@@ -1,5 +1,5 @@
 '''
-Complete exercises 8.5 and 8.6
+Complete exercises in section 8.7 (p.75)
 
 CODE:
 
@@ -8,12 +8,12 @@ count = 0
 for letter in word:
     if letter == 'a':
         count = count + 1
-print count
+print(count)
 
-8.5 - Encapsulate this code in a function named count,
+1) - Encapsulate this code in a function named count,
 and generalize it so that it accepts the string and the letter as arguments.
 
-8.6 - Rewrite this function so that instead of traversing the string,
+2) - Rewrite this function so that instead of traversing the string,
 it uses the three-parameter version of find from the previous section.
 
 '''
