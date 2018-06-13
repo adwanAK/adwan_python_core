f90f7d7a5c2c7297f2d745a90e69cf8b9a6a1985
Martin Breuss <martin-martin@users.noreply.github.com>
2018-06-13 23:47:19 +0200

    update to p3 book numbers

--- week_03/labs/08_tuples/Exercise_02.py
+++ week_03/labs/08_tuples/Exercise_02.py
@@ -1,5 +1,5 @@
 '''
-Complete Exercise 12.1 from the textbook (p. 115).
+Complete the task in section 12.4 - Variable-length argument tuples - (p. 118).
 
 Write a function called sumall that takes any number of arguments
 and returns their sum.
