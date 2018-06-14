11c8747bd061a3451d4a20ddd422e3530845803b
martin-martin <breuss.martin@gmail.com>
2018-06-14 23:20:48 +0200

    update instructions

--- week_03/labs/09_dictionaries/Exercise_01.py
+++ week_03/labs/09_dictionaries/Exercise_01.py
@@ -1,10 +1,11 @@
 '''
-Complete Exercise 11.3 from the textbook (p. 104).
+Starting from Exercise 11.3 from the textbook (p. 106):
 
-Modify print_hist to print the keys and their values in alphabetical order.
+Modify print_hist to print the keys and their values in alphabetical order
+without using the sorted() function.
 
 def print_hist(h):
     for c in h:
-        print c, h[c]
+        print(c, h[c])
 
 '''
