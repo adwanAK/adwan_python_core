19849642ae246ffb740ed8a6b95deb3c666eacbe
martin-martin <breuss.martin@gmail.com>
2018-08-17 15:06:30 +0200

    add project

---
+++ week_04/mini_projects/wisdom_speaker/speaker.py
@@ -0,0 +1,11 @@
+'''
+Use the external python module 'markovify' to create short text snippets
+of your favorite text resource.
+
+https://github.com/jsvine/markovify
+
+CHALLENGE: rewrite it using `markovbot` to let the bot tweet
+some wisdom once in a while!
+https://github.com/esdalmaijer/markovbot
+
+'''
