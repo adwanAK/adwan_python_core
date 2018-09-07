a16c9ac5be0a342ea67ac2d5a88d376c3d9e36b2
Martin Breuss <martin-martin@users.noreply.github.com>
2018-09-07 23:57:57 +0200

    update instructions

--- week_02/mini_projects/lyrics.py
+++ week_02/mini_projects/lyrics.py
@@ -6,6 +6,8 @@
 Programmatically create your own song lyrics with multiple verses,
 interlaced with a repeating chorus.
 
+Train using string methods and loops on an open-end mini-project!
+
 - use one block of text as verse input (hint: linebreaks can be helpful!)
 - use a for loop for creating the full lyrics
 
