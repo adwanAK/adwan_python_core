86f05f0f48ea1e0d5876ed91871a36acbab4adb9
Adwan <adwan999@yahoo.com>
2018-09-12 13:42:18 +0100

    udpated from redesmond okay

--- week_02/labs/04_conditionals_loops/Exercise_04.py
+++ week_02/labs/04_conditionals_loops/Exercise_04.py
@@ -12,10 +12,10 @@
 print("Prime numbers between 1 and 100 are:")
 
 for num in range(1,100 + 1):
-   # prime numbers are greater than 1
-   if num > 1:
-       for i in range(2,num):
-           if (num % i) == 0:
-               break
-       else:
-           print(num)
+    # prime numbers are greater than 1
+    if num > 1:
+        for i in range(2,num):
+            if (num % i) == 0:
+                break
+            else:
+                print(num)
