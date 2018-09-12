27bcc30bcf1a8e3ff4bb27e4ddf5ae84e9144574
Caden MacKenzie <cadenmackenzie@gmail.com>
2018-09-12 12:43:15 +0200

    updated page numbers for string exercises

--- week_03/labs/06_strings/Exercise_03.py
+++ week_03/labs/06_strings/Exercise_03.py
@@ -1,4 +1,4 @@
 '''
-Complete Exercise 8.4 (p.80) from the textbook by writing out the docstrings for the functions.
+Complete Exercise 8.4 (p.96) from the textbook by writing out the docstrings for the functions.
 
 '''
