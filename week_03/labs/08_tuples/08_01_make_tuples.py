65167e4acbbe07b94f8585ee27c275ee14c59a72
martin-martin <breuss.martin@gmail.com>
2018-09-13 12:59:32 +0200

    update 08 labs

---
+++ week_03/labs/08_tuples/08_01_make_tuples.py
@@ -0,0 +1,12 @@
+'''
+Write a script that takes in a list of numbers and:
+    - sorts the numbers
+    - stores the numbers in tuples of two in a list
+    - prints each tuple
+
+
+Notes:
+If the user enters an odd numbered list, add the last item
+to a tuple with the number 0.
+
+'''
---
+++ week_03/labs/08_tuples/08_02_word_frequencies.py
@@ -0,0 +1,12 @@
+'''
+Write a function called most_frequent that takes a string and prints
+the letters in decreasing order of frequency. Find text samples from
+several different languages and see how letter frequency varies between
+languages. Compare your results with the tables at:
+http://en.wikipedia.org/wiki/Letter_frequencies.
+Solution: http://thinkpython2.com/code/most_frequent.py.
+
+Source: Chapter on "Tuples" in Think Python 2e:
+http://greenteapress.com/thinkpython2/html/thinkpython2013.html
+
+'''
