f835ce5c35653f537d54264274c195267f617581
martin-martin <breuss.martin@gmail.com>
2018-09-13 12:23:15 +0200

    update 11 labs

---
+++ week_03/labs/11_inheritance/11_01_subclasses.py
@@ -0,0 +1,18 @@
+'''
+Build on 10_03_freeform_classes from the section on Classes, Objects and
+Methods.
+Create subclasses of two of the existing classes. Create a subclass of
+one of those so that the hierarchy is at least three levels.
+
+Build these classes out like we did in 10_03_freeform_classes.
+
+If you cannot think of a way to build on your previous exercise,
+you can start from scratch here.
+
+We encourage you to be creative and try to think of an example of
+your own for this exercise but if you are stuck, some ideas include:
+
+- A Vehicle superclass, with Truck and Motorcycle subclasses.
+- A Restaurant superclass, with Gourmet and FastFood subclasses.
+
+'''
