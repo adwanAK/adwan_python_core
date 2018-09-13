f367552258b3cf1bc1ad928207ecb6384e7922d7
martin-martin <breuss.martin@gmail.com>
2018-09-13 12:14:58 +0200

    update 10 labs

---
+++ week_03/labs/10_classes_objects_methods/10_02_points.py
@@ -0,0 +1,8 @@
+'''
+Work through the chapter "Classes and Objects" in Think Python 2e:
+http://greenteapress.com/thinkpython2/html/thinkpython2016.html and
+build out the Point class example.
+
+The provided code to start is in file Point1.py in this folder.
+
+'''
