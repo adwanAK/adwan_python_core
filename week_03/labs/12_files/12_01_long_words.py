bab69da771f1b68b3e058df5290df6ef3d945080
Adwan <adwan999@yahoo.com>
2018-09-18 00:04:39 +0100

    Completed first and second labs form Files

--- week_03/labs/12_files/12_01_long_words.py
+++ week_03/labs/12_files/12_01_long_words.py
@@ -5,3 +5,15 @@ words with more than 20 characters (not counting whitespace).
 Source: http://greenteapress.com/thinkpython2/html/thinkpython2010.html
 
 '''
+
+with open('words.txt', 'r') as f:
+    data = f.read()
+
+    # create a list of word by splitting by EOL End Of Line
+    data_list = data.split("\n")
+
+    # Loop through the list and print words with characters > 20
+    for word in data_list:
+        if len(word) >= 20:
+            print(word)
+
--- week_03/labs/12_files/12_02_rename_doc.py
+++ week_03/labs/12_files/12_02_rename_doc.py
@@ -15,3 +15,55 @@ Source: Read through the "Files" chapter in Think Python 2e:
 http://greenteapress.com/thinkpython2/html/thinkpython2015.html
 
 '''
+def sed (pattern, replacement, input_file, output_file):
+    output_string = read_file(input_file)
+    write_file(output_string.replace(pattern, replacement), output_file)
+
+
+def read_file(input_file):
+    try:
+        f= open(input_file, 'r')
+        data = f.read()
+        f.close()
+        print(f"Read completed successfully from file '{input_file}'")
+        return data
+    except IOError:
+        print('An error occured trying to read the file.')
+
+    except EOFError:
+        print('Why did you do an EOF on me?')
+
+    except KeyboardInterrupt:
+        print('You cancelled the operation.')
+
+    except:
+        print('An error occured.')
+
+
+
+def write_file(output_string, output_file):
+    try:
+        f= open(output_file, 'w')
+        f.write(output_string)
+        f.close()
+        print(f"Write completed successfully to file '{output_file}'")
+    except IOError:
+        print('An error occured trying to read the file.')
+
+    except EOFError:
+        print('Why did you do an EOF on me?')
+
+    except KeyboardInterrupt:
+        print('You cancelled the operation.')
+
+    except:
+        print('An error occured.')
+
+
+############## End of code!  below is for resting script only##########
+if __name__ == '__main__':
+    # Delete output file content just to make sure it's empty, for testing over and over again
+    write_file("", "output_file_for_12_02.txt")
+
+    # Call the sed function to replace pattern and write to output file
+    sed("Name", "Adwan", "input_file_for_12_02.txt", "output_file_for_12_02.txt")
